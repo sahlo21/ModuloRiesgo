2e2f5366305104c9d5c047092f1c7d6adaed69bb
sahlo21 <kssm21p@gmail.com>
2023-04-21 21:20:01 -0500

    Interfaces 100% v1.2

--- src/co/uniquindio/edu/co/moduloriesgo/controller/PlanRiesgoController.java
+++ src/co/uniquindio/edu/co/moduloriesgo/controller/PlanRiesgoController.java
@@ -1,5 +1,24 @@
 package co.uniquindio.edu.co.moduloriesgo.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+
 public class PlanRiesgoController {
+
+    @FXML
+    private TextArea txtPlan;
+
+    @FXML
+    private DatePicker dateFin;
+
+    @FXML
+    private DatePicker dateInicio;
+
+    @FXML
+    void generarPlanEvent(ActionEvent event) {
+
+    }
 
 }
